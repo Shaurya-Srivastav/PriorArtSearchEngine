39564a513bd87bd76ec3b1248d1749ed7ab25ff7
Shaurya-Srivastav <ssrivastav@ucdavis.edu>
2023-08-14 19:07:41 +0530

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { ResultsComponent } from './results/results.component';
 
 const routes: Routes = [
-  { path:'', redirectTo:"home", pathMatch:"full"},
-  { path : 'home' , component:HomeComponent}, 
+  { path:'', redirectTo:"search", pathMatch:"full"},
   { path : 'search', component:SearchComponent}, 
-  { path : 'results', component:ResultsComponent}
+  { path : 'results', component:ResultsComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 
 import { HttpClientModule } from "@angular/common/http";
 
@@ -19,9 +19,8 @@ import { ResultsComponent } from './results/results.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     SearchComponent,
-    ResultsComponent
+    ResultsComponent, 
   ],
   imports: [
     BrowserModule,
