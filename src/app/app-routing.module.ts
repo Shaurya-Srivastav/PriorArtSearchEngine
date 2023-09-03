cf221d6d78decae2e316d6f74627879339d111cf
Shaurya-Srivastav <ssrivastav@ucdavis.edu>
2023-09-02 19:18:14 -0700

    saving changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { ResultsComponent } from './results/results.component';
+import { SavedComponent } from './saved/saved.component';
 
 const routes: Routes = [
   { path:'', redirectTo:"search", pathMatch:"full"},
   { path : 'search', component:SearchComponent}, 
   { path : 'results', component:ResultsComponent},
+  { path: 'saved', component:SavedComponent}, 
 ];
 
 @NgModule({
