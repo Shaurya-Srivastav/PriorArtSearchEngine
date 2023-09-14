af24b56bd90b5411d3f582ca455e680f9be45022
Shaurya-Srivastav <ssrivastav@ucdavis.edu>
2023-09-14 14:13:20 -0700

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,41 +1,22 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, AfterViewInit {
-  @ViewChild('loginBox') loginBox!: ElementRef;
-  @ViewChild('signupBox') signupBox!: ElementRef;
-  @ViewChild('loginLink') loginLink!: ElementRef;
-  @ViewChild('signupLink') signupLink!: ElementRef;
+export class LoginComponent implements OnInit {
+  showSignupForm: boolean = false;
+  username: string = '';
+  password: string = '';
+  newUsername: string = '';
+  newPassword: string = '';
 
   constructor() {}
 
   ngOnInit() {}
 
-  ngAfterViewInit() {
-    // Get references to the anchor elements
-    const signupLinkElement = this.signupLink.nativeElement;
-    const loginLinkElement = this.loginLink.nativeElement;
-
-    // Handle click on "Sign up" link
-    signupLinkElement.addEventListener('click', (event: MouseEvent) => {
-      event.preventDefault(); // Prevent the default behavior of the anchor
-      this.loginBox.nativeElement.style.display = 'none';
-      this.signupBox.nativeElement.style.display = 'block';
-    });
-
-    // Handle click on "Back to Login" link
-    loginLinkElement.addEventListener('click', (event: MouseEvent) => {
-      event.preventDefault(); // Prevent the default behavior of the anchor
-      this.signupBox.nativeElement.style.display = 'none';
-      this.loginBox.nativeElement.style.display = 'block';
-    });
-
-    // Handle form submission (you can add form submission logic here)
-    // Example: this.loginForm.nativeElement.addEventListener('submit', (e) => { ... });
-    // Example: this.signupForm.nativeElement.addEventListener('submit', (e) => { ... });
+  toggleForm() {
+    this.showSignupForm = !this.showSignupForm;
   }
 }
